25f67f43329a1628fbd612d7633e88ef4af3e86c
HeeteshSimon <heeteshsimon@gmail.com>
2020-04-21 12:48:17 +0530

    Added few functions

--- Tic Tac Toe.py
+++ Tic Tac Toe.py
@@ -5,8 +5,13 @@ def display_board():
     print(board[0]+ '|'+board[1]+'|'+board[2])
     print(board[3] + '|' + board[4] + '|' + board[5])
     print(board[6] + '|' + board[7] + '|' + board[8])
-display_board()
-
+def playgame():
+    #Display Initial Board
+    display_board()
+    handleturn()
+def handleturn():
+    position=input('Choose your position between 1-9: ')
+playgame()
 
 
 
